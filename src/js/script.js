d8dfc6a744ae68d0e10ff4addd0076643398ba2f
Ilya <ilya070105@gmail.com>
2024-03-07 15:14:24 +0300

    slider pre adaptiv version

--- src/js/script.js
+++ src/js/script.js
@@ -1,28 +1,17 @@
-/* $(document).ready(function(){
+$(document).ready(function(){
   $('.carousel__inner').slick({
-    speed: 1200,
-    adaptiveHeight: true,
-    prevArrow: '<button type="button" class="slick-prev"><img src="../src/icons/left.svg"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="../src/icons/right.svg"></button>',
-    responsive: [
-      {
-        breakpoint: 992,
-        settings: {
-          dots: true,
-          arrows: false
-        }
-      }
-    ]
+      speed: 1200,
+      adaptiveHeight: true,
+      prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+      responsive: [
+          {
+              breakpoint: 992,
+              settings: {
+                  dots: true,
+                  arrows: false
+              }
+          }
+      ]
   });
-}); */
-var slider = tns({
-  container: '.carousel__inner',
-  items: 1,
-  slideBy: 'page',
-  autoplay: false,
-  controlsText: [
-    '<img src="../src/icons/left.svg">',
-    '<img src="../src/icons/right.svg">'
-
-  ]
 });
