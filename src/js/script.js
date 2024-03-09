fbaf980314b5277315682da7a01b7dead78d4551
Ilya <ilya070105@gmail.com>
2024-03-09 10:50:49 +0300

    1123

--- src/js/script.js
+++ src/js/script.js
@@ -4,14 +4,38 @@ $(document).ready(function(){
       adaptiveHeight: true,
       prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
       nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+      slidesToShow: 1,
+      slidesToScroll: 1,
       responsive: [
-          {
-              breakpoint: 992,
-              settings: {
-                  dots: true,
-                  arrows: false
-              }
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true,
+            arrows: false
           }
-      ]
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: true
+
+          }
+        },
+        {
+          breakpoint: 575,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true
+
+          }
+        }
+      ] 
+    });
   });
-});
