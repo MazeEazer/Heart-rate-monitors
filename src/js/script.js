9da1a520a823a7f8c7d23164c40a178dc2c70af1
Ilya <ilya070105@gmail.com>
2024-03-23 10:58:52 +0300

    animations

--- src/js/script.js
+++ src/js/script.js
@@ -78,4 +78,52 @@ $(document).ready(function(){
       $('.overlay,#order').fadeIn('slow');
     });
   });
+
+
+  function valideForms(form){
+    $(form).validate({
+      rules: {
+        name:{
+              required: true,
+              minlength: 2
+        }, 
+        phone: "required",
+        email: {
+          required: true,
+          email: true
+        }
+      },
+      messages: {
+        name: {
+          required: "Пожалуйста, введите свое имя",
+          minlength: jQuery.validator.format("Введите {0} символа!")
+        },
+        phone: "Пожалуйста, введите свой номер телефона",
+        email: {
+          required: "Пожалуйста, введите свою почту",
+          email: "Неправильно введен адрес почты"
+        }
+      }
+    });
+  }
+  valideForms('#consultation-form');
+  valideForms('#consultation form');
+  valideForms('#order form');
+
+  $('input[name=phone]').mask("+7 (999) 999-99-99")
+  //smooth scroll and pageup
+
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 1600){
+      $('.pageup').fadeIn();
+    } else {
+      $('.pageup').fadeOut();
+    }
+  });
+
+  $("a[href^='#']").click(function(){
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offstet().top+"px"}); 
+    return false;
+  });
 });
